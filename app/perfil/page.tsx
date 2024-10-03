dbc209d444fb0ac59e26515e02477e7960b7c75d
Felipe Rioske <rioskenakamura@gmail.com>
2024-10-03 14:16:54 -0400

    perfil

---
+++ app/perfil/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useState } from "react";
+import Usuario from "../interfaces/usuario";
+
+const PaginaPerfil = () => {
+({nome: "Rioske Nakamura", email: "rioskenakamura@gmail", idade: 20})
+   
+   
+    /*const usuario = {
+        nome: "Rioske Nakamura",
+        email: "rioskenakamura@gmail",
+        idade: 20
+    }*/
+
+    return (
+        <div>
+            <h1>Perfil</h1>
+           
+        </div>
+    )
+}
+
+export default PaginaPerfil
